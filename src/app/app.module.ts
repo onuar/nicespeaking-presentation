f7187f92987b0798aa6d9a442280112582c7df9d
Onur Aykac <onuraykac@gmail.com>
2016-12-12 21:47:32 +0200

    new and list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NewItemComponent } from './new-item/new-item.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewItemComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
